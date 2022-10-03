8bd29575a4495e1a9d3dce3876b8321d9537fd33
fernando_carrasco <fernandocarrasc590@gmail.com>
2022-10-02 21:47:39 -0400

    7 configurando jetstream

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,27 +19,27 @@ class UserSeeder extends Seeder
         $user1 = new User();
         $user1->name = "fernando";
         $user1->email = "fer@gmail.com";
-        $user1->password = bcrypt("12345678");
+        $user1->password = bcrypt("123");
         $user1->save();
 
         //Fotografo
         $user2 = new User();
         $user2->name = "idalia";
         $user2->email = "ida@gmail.com";
-        $user2->password = bcrypt("12345678");
+        $user2->password = bcrypt("123");
         $user2->save();
 
         //Cliente
         $user3 = new User();
         $user3->name = "marco";
         $user3->email = "marc@gmail.com";
-        $user3->password = bcrypt("12345678");
+        $user3->password = bcrypt("123");
         $user3->save();
 
         $user4 = new User();
         $user4->name = "pepe";
         $user4->email = "pepe@gmail.com";
-        $user4->password = bcrypt("12345678");
+        $user4->password = bcrypt("123");
         $user4->save();
 
     }
