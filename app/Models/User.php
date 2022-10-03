055729f581fd48e49e28c14985f05f7e42cb3476
fernando_carrasco <fernandocarrasc590@gmail.com>
2022-10-02 21:07:34 -0400

    6 instalado jetstream para login con tailwind

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,21 +2,26 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
-    protected $table = "users";
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var string[]
      */
     protected $fillable = [
         'name',
@@ -27,41 +32,30 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
     ];
 
     /**
      * The attributes that should be cast.
      *
-     * @var array<string, string>
+     * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
 
-    public function telefonos(){
-        $telefonos = Telefono::where('user_id',$this->id)->get();
-        return $telefonos;
-    }
-
-    // Un usuario no puede ser los 3 a la vez, devuelve null si no es un tipo
-    public function fotografo(){
-        $fotografo = Fotografo::where('user_id',$this->id)->first();
-        return $fotografo;
-    }
-
-    public function organizador(){
-        $organizador = Organizador::where('user_id',$this->id)->first();
-        return $organizador;
-    }
-
-    public function cliente(){
-        $cliente = Cliente::where('user_id',$this->id)->first();
-        return $cliente;
-    }
-    
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
 }
